7049d60fa263335190cd1b07ec29813fcca8ad6d
shaw <1939484341@qq.com>
2022-10-25 18:11:42 +0800

    ffmpeg 播放器

---
+++ WpfVideoFfmpegPlayer/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace WpfVideoFfmpegPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UrlTextBox.Text = @"https://v2.kwaicdn.com/upic/2022/09/30/12/BMjAyMjA5MzAxMjEyNDJfMjIxMjQ4ODIyNV84NTI5MzA3Mzg3MV8yXzM=_hd15_B2e9c58667813baf16b5acf3ac0f2eea1.mp4?pkey=AAXhc_e-EnF4H0GiBMdZePwU8P-6KUrYEQck8gYFHJN3dWHTX9e53UpCqaHMHGw5VqgL7mm3zFIifaWb5GnSS_8H3vC-7y2fS7gA08Qr-_q-nXpgJBbJchSZQ4CukMdm6Ok&tag=1-1666689207-unknown-0-3wkszwowoe-97998829cd9c57c6&clientCacheKey=3xgtkejfdmn7a5k_hd15.mp4&di=3afe9535&bp=10004&tt=hd15&ss=vp";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //var sourceStr = @"https://v2.kwaicdn.com/upic/2022/09/30/12/BMjAyMjA5MzAxMjEyNDJfMjIxMjQ4ODIyNV84NTI5MzA3Mzg3MV8yXzM=_hd15_B2e9c58667813baf16b5acf3ac0f2eea1.mp4?pkey=AAXhc_e-EnF4H0GiBMdZePwU8P-6KUrYEQck8gYFHJN3dWHTX9e53UpCqaHMHGw5VqgL7mm3zFIifaWb5GnSS_8H3vC-7y2fS7gA08Qr-_q-nXpgJBbJchSZQ4CukMdm6Ok&tag=1-1666689207-unknown-0-3wkszwowoe-97998829cd9c57c6&clientCacheKey=3xgtkejfdmn7a5k_hd15.mp4&di=3afe9535&bp=10004&tt=hd15&ss=vp";
+            //this.player1.Play(sourceStr);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var urlPath = UrlTextBox?.Text;
+            this.player1.Play(urlPath);
+        }
+    }
+}
---
+++ WpfVideoFfmpegPlayer/Player/PlayerControl.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfVideoFfmpegPlayer.Player
+{
+    /// <summary>
+    /// Interaction logic for PlayerControl.xaml
+    /// </summary>
+    public partial class PlayerControl : UserControl
+    {
+        public PlayerControl()
+        {
+            this.Dispatcher.BeginInvoke((Action)delegate {
+                InitializeComponent();
+
+                //必须在为播放器指定uri之前设置FFmpegDirectory的值
+                //它引用一些依赖的.dll和.exe文件，该文件在../bin/DLL/中
+                Unosquare.FFME.MediaElement.FFmpegDirectory = Directory.GetCurrentDirectory() + "\\DLL\\";
+
+                //当视频播放器的属性发生改变时，如开始播放，停止播放
+                //同时修改由播放状态决定的属性值
+                // Media.PropertyChanged += Media_PropertyChanged;
+
+                this.Unloaded += PlayerControl_Unloaded;
+            });
+        }
+
+        private void PlayerControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Media?.Close();
+        }
+
+        public void Play(string url)
+        {
+            if (Media == null)
+                return;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(url))
+                {
+                    if (Media.IsPlaying)
+                        Media.Stop();
+                    //指定播放路径
+                    Media.Source = new Uri(url);
+                    var aaa = Media.IsPlaying;
+                    //开始播放
+                    Media.Play();
+                    var bbb = Media.IsPlaying;
+                }
+            }
+            catch /*(Exception e)*/
+            {
+                //Console.WriteLine(e);
+                //throw;
+            }
+        }
+    }
+}
