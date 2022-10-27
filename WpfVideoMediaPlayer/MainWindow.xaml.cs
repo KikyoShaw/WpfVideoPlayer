c3745ffe88c9851b7d43fd7b9e46e79c0aceda0b
shaw <1939484341@qq.com>
2022-10-27 15:09:07 +0800

    WPF 测试 MediaElement内存问题

---
+++ WpfVideoMediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfVideoMediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UrlTextBox.Text = @"http://v-platform-video-gz-test.cdn.huya.com/leaf/1048585/1099511667844/2222653/2689_936b91657a34bb471eb09c128aa777f2_264_1080.mp4";
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = UrlTextBox.Text;
+            if(string.IsNullOrEmpty(text))
+                return;
+            VideoRender.Play(text);
+        }
+
+        private void ButtonBase1_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = UrlTextBox.Text;
+            if (string.IsNullOrEmpty(text))
+                return;
+            VideoRender.Restart(text);
+        }
+
+        private void ButtonBase2_OnClick(object sender, RoutedEventArgs e)
+        {
+            VideoRender.Stop();
+        }
+    }
+}
---
+++ WpfVideoMediaPlayer/VideoRender.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfVideoMediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for VideoRender.xaml
+    /// </summary>
+    public partial class VideoRender : UserControl
+    {
+        private readonly MediaPlayer _player;
+        public VideoRender()
+        {
+            InitializeComponent();
+            _player = new MediaPlayer();
+            //player.Open(new Uri(@"test.mp4", UriKind.Relative));
+            //player.Play();
+            _player.Volume = 0;
+        }
+
+        public void Play(string text)
+        {
+            _player?.Open(new Uri(text));
+            _player?.Play();
+            this.InvalidateVisual();
+        }
+
+        public void Pause()
+        {
+            _player?.Pause();
+        }
+
+        public void Stop()
+        {
+            _player?.Stop();
+        }
+
+        public void Restart(string text)
+        {
+            _player?.Stop();
+            _player?.Open(new Uri(text));
+            _player?.Play();
+            this.InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            if (_player != null && _player.Source != null)
+            {
+                //var height = _player.NaturalVideoHeight * this.ActualWidth / _player.NaturalVideoWidth;
+                drawingContext.DrawVideo(_player, new Rect(0,0, 500, 700/*this.ActualWidth, height*/));
+            }
+        }
+    }
+}
